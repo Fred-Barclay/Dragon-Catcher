0099e4d5f2bb5a1bfbbcc7f48eb9bf253e16604f
Fred-Barclay <Fred-Barclay@users.noreply.github.com>
2016-10-11 17:42:15 -0500

    sigh. Hopefully this  (temporarily) fixes things.

--- src/dragon-catcher.py
+++ src/dragon-catcher.py
@@ -135,8 +135,7 @@ else:
 # Is apache2 running?
 p1 = subprocess.Popen(['pidof', 'apache2'], stdout=subprocess.PIPE).communicate()[0]
 if not len(p1.split()) == 0:
-	print('There is an actively running instance of apache2. You may want to \
-		investigate the cause. \x1b[6;36;43m [Advice] \x1b[0m')
+	print('There is an actively running instance of apache2. You may want to investigate the cause. \x1b[6;36;43m [Advice] \x1b[0m')
 	print(p1.split())
 	total_advice.append('apache2_running_FAIL')
 else:
