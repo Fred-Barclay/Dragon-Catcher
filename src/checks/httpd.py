77b954a0e0978a43d3e20011efccdb5c501c7b8a
Fred-Barclay <Fred-Barclay@users.noreply.github.com>
2016-10-14 13:27:40 -0500

    fixed typo

--- src/checks/httpd.py
+++ src/checks/httpd.py
@@ -8,7 +8,7 @@ def httpd():
 	'''Detect an actively running instance of httpd.'''
 	p1 = subprocess.Popen(['pidof', 'httpd'], stdout=subprocess.PIPE).communicate()[0]
 	if not len(p1.split()) == 0:
-		print('There is an actively running instance of httpd.You may want'
+		print('There is an actively running instance of httpd.You may want')
 		print('to investigate the cause. \x1b[6;36;43m [Advice] \x1b[0m')
 		print(p1.split())
 	else:
