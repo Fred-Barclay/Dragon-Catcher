f9f9dd2f8c8b59d6a1d83151e555247133193a44
Fred-Barclay <Fred-Barclay@users.noreply.github.com>
2016-10-14 13:25:20 -0500

    Improved error message formatting

--- src/checks/apache2.py
+++ src/checks/apache2.py
@@ -8,10 +8,8 @@ def apache2():
 	'''Detect an actively running instance of apache 2'''
 	p1 = subprocess.Popen(['pidof', 'apache2'], stdout=subprocess.PIPE).communicate()[0]
 	if not len(p1.split()) == 0:
-		print('''
-		There is an actively running instance of apache2. You may want to
-		investigate the cause. \x1b[6;36;43m [Advice] \x1b[0m
-		''')
+		print('There is an actively running instance of apache2. You may want')
+		print('to investigate the cause. \x1b[6;36;43m [Advice] \x1b[0m')
 		print(p1.split())
 	else:
 		print('There are not detected instances of apache2.')
