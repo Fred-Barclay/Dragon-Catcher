dfe5916312bd6df01eff81e533f1965d1cccb54a
Fred Barclay <Fred-Barclay@users.noreply.github.com>
2017-04-01 20:23:48 -0500

    syntax correction

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # run.sh
 # Copyright (C) 2016-2017 Dragon Catcher authors
 # This file is distributed under the same license as the Dragon Catcher package.
-cd $(pwd)/src/
+cd "$(pwd)/src/"
 sudo python3 dragon-catcher.py
 sudo rm -rf checks/__pycache__
